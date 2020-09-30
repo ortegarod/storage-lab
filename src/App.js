6f1c4ff56d44a3bd39130be94ce347af60b00287
Rodrigo Ortega <ortegarod000@gmail.com>
2020-09-30 00:07:21 -0500

    working on locahost:3000

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import {simpleStorage} from "./abi/abi";
+import Web3 from "web3";
+import "./App.css";
+
+const web3 = new Web3(Web3.givenProvider);
+
+const contractAddress = "0x32faA7a3c1eaEF2Ce8eb14Dbbf3A6920af394535";
+const simpleContract = new web3.eth.Contract(simpleStorage, contractAddress);
 
 function App() {
+  const [number, setUint] = useState(0);
+  const [getNumber, setGet] = useState("0");
+
+  const numberSet = async (t) => {
+    t.preventDefault();
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    const gas = await simpleContract.methods.set(number).estimateGas();
+    const post = await simpleContract.methods.set(number).send({
+      from: account,
+      gas,
+    });
+  };
+ 
+  const numberGet = async (t) => {
+    t.preventDefault();
+    const post = await simpleContract.methods.get().call();
+    setGet(post);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="main">
+        <div className="card">
+          <form className="form" onSubmit={numberSet}>
+            <label>
+              Set your uint256:
+              <input
+                className="input"
+                type="text"
+                name="name"
+                onChange={(t) => setUint(t.target.value)}
+              />
+            </label>
+            <button className="button" type="submit" value="Confirm">
+              Confirm
+            </button>
+          </form>
+          <br />
+          <button className="button" onClick={numberGet} type="button">
+            Get your uint256
+          </button>
+          {getNumber}
+        </div>
+      </div>
   );
 }
 
